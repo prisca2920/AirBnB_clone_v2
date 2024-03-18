1cd904675b17c3ba7500d9bb58a3fcfe1d37a5ee
prisca2920 <prisca2920@gmail.com>
2024-03-18 10:14:34 -0700

    sql

--- setup_mysql_dev.sql
+++ setup_mysql_dev.sql
@@ -1,4 +1,4 @@
---prepares a mysql server
+-- prepares a mysql server
 CREATE DATABASE IF NOT EXISTS hbnb_dev_db;
 CREATE USER IF NOT EXISTS 'hbnb_dev'@'localhost' IDENTIFIED BY 'hbnb_dev_pwd';
 GRANT ALL PRIVILEGES ON hbnb_dev_db.* TO 'hbnb_dev'@'localhost';
