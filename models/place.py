14432c1ba0c130cbf87ac3e4f794bbd0912f00b3
james-eo <jameseokonkwo@gmail.com>
2024-03-21 05:29:41 +0100

    Place updated

--- models/place.py
+++ models/place.py
@@ -36,8 +36,8 @@ class Place(BaseModel, Base):
     number_bathrooms = Column(Integer, nullable=False, default=0)
     max_guest = Column(Integer, nullable=False, default=0)
     price_by_night = Column(Integer, nullable=False, default=0)
-    latitude = Column(Float, nullable=True, default=0.0)
-    longitude = Column(Float, nullable=True, default=0.0)
+    latitude = Column(Float, nullable=True)
+    longitude = Column(Float, nullable=True)
     amenity_ids = []
     if os.environ.get('HBNB_TYPE_STORAGE') == 'db':
         reviews = relationship("Review",
