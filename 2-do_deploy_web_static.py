645553346017a0a5037ad1c535aca65fe1e5ac8e
prisca2920 <prisca2920@gmail.com>
2024-04-04 11:52:12 -0700

    distrstributes an archive to the server

---
+++ 2-do_deploy_web_static.py
@@ -0,0 +1,30 @@
+#!/usr/bin/python3
+"""distributes an archive to web servers"""
+import os
+from fabric.api import env, put, run
+
+env.hosts = ['18.206.206.189', '54.86.9.156']
+
+
+def do_deploy(archive_path):
+    """distributes an archive to web servers"""
+    if not os.path.exists(archive_path):
+        return False
+    file_name = os.path.basename(archive_path)
+    folder_name = file_name.replace(".tgz", "")
+    folder_path = "/data/web_static/releases/{}/".format(folder_name)
+    success = False
+    try:
+        put(archive_path, "/tmp/{}".format(file_name))
+        run("mkdir -p {}".format(folder_path))
+        run("tar -xzf /tmp/{} -C {}".format(file_name, folder_path))
+        run("rm -rf /tmp/{}".format(file_name))
+        run("mv {}web_static/* {}".format(folder_path, folder_path))
+        run("rm -rf {}web_static".format(folder_path))
+        run("rm -rf /data/web_static/current")
+        run("ln -s {} /data/web_static/current".format(folder_path))
+        print('New version deployed!')
+        success = True
+    except Exception:
+        success = False
+    return success
